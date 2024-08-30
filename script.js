8aa79889f8aff43d35c08a3ad9cd2ea8f489129b
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2024-08-30 12:02:08 +0200

    Add pseudocode

---
+++ script.js
@@ -0,0 +1,9 @@
+//How will Champions League simulator work
+
+//Step 1: Create objects of n teams
+
+//step 2: create 4 pot arrays, each containing n/4 team objects
+
+//step 3: start the draw, randomly select a team from all the pots.
+
+//step 4: for that team, randomly select n/4 team that they will play, exluding themseles. ranmly selecting wheher home or away. 
