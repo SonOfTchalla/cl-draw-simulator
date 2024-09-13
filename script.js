639b0ea8f409914a5dd72c62e9ba32f472b9814f
SonOfTchalla <121317931+SonOfTchalla@users.noreply.github.com>
2024-09-13 10:54:45 +0200

    Add an array of objects for teams
    Create an array of 36 objects, each object representing a team, containg the team name, which pot the team is in and the number of home and way games, both set to default as 0.

--- script.js
+++ script.js
@@ -1,7 +1,224 @@
 //How will Champions League simulator work
 
 //Step 1: Create objects of n teams
-
+let teams = [
+    {
+        "name" : "REAL MADRID",
+        "Pot" : 1,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "MANCHESTER CITY",
+        "Pot" : 1,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "BAYERN MUNCHEN",
+        "Pot" : 1,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "PARIS SAINT-GERMAIN",
+        "Pot" : 1,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "LIVERPOOL",
+        "Pot" : 1,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "INTER",
+        "Pot" : 1,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "DORTMUND",
+        "Pot" : 1,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "LEIPZIG",
+        "Pot" : 1,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "BARCELONA",
+        "Pot" : 1,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "LEVERKUSEN",
+        "Pot" : 2,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "ATLETICO DE MADRID",
+        "Pot" : 2,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "ATALANTA",
+        "Pot" : 2,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "JUVENTUS",
+        "Pot" : 2,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "BENFICA",
+        "Pot" : 2,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "ARSENAL",
+        "Pot" : 2,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "CLUB BRUGGE",
+        "Pot" : 2,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "SHAKHTAR DONETSK",
+        "Pot" : 2,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "AC MILAN",
+        "Pot" : 2,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "FEYENOORD",
+        "Pot" : 3,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "SPORTING CP",
+        "Pot" : 3,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "PSV EINDHOVEN",
+        "Pot" : 3,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "GNK DINAMO",
+        "Pot" : 3,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "SALZBURG",
+        "Pot" : 3,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "LILLE",
+        "Pot" : 3,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "CRVENA ZDVEZDA",
+        "Pot" : 3,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "YOUNG BOYS",
+        "Pot" : 3,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "CELTIC",
+        "Pot" : 3,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "SLOVAN BRATISLAVA",
+        "Pot" : 4,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "MONACO",
+        "Pot" : 4,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "SPARTA PRAHA",
+        "Pot" : 4,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "ASTON VILLA",
+        "Pot" : 4,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "BOLOGNA",
+        "Pot" : 4,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "GIRONA",
+        "Pot" : 4,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "STUTTGART",
+        "Pot" : 4,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "STRUM GRAZ",
+        "Pot" : 4,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+    {
+        "name" : "BREST",
+        "Pot" : 4,
+        "homeGames": 0,
+        "awayGames": 0
+    },
+]
 //step 2: create 4 pot arrays, each containing n/4 team objects
 
 //step 3: start the draw, randomly select a team from all the pots.
